d84a806b2c9a8b72c995499150376c859ca0ed45
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2025-11-13 00:20:59 +0530

    updated exceptions file

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -4,4 +4,4 @@ from fastapi.responses import JSONResponse
 def register_exception_handlers(app: FastAPI):
     @app.add_exception_handler(Exception)
     async def unhanded_exception_handler(request: Request, exc: Exception):
-        return JSONResponse(status_code=500, content={'detail': str(exc)})
+        return JSONResponse(status_code=500, content={'error': str(exc)})
