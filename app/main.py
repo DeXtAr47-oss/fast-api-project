2b4361d809ef035526282230dd089e38fa54cf6f
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2025-11-22 20:35:35 +0530

    updated main.py file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 
-from ..app.api import routes_auth, routes_predict
-from ..app.middleware.logging_middleware import LoggingMiddleware
-from ..app.core.exceptions import register_exception_handlers
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.core.exceptions import register_exception_handlers
 
 app = FastAPI(title='car price prediction')
 
